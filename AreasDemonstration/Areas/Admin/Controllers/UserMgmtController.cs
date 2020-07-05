ea18561e0a831ad8a734f9835e45194928879eeb
Utpal Maiti <Utpalmaiti12@gmail.com>
2020-07-05 22:37:54 +0530

    Add project files.

---
+++ AreasDemonstration/Areas/Admin/Controllers/UserMgmtController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AreasDemonstration.Areas.Admin.Controllers
+{
+    public class UserMgmtController : Controller
+    {
+        // GET: Admin/UserMgmt
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ApproveUsers()
+        {
+            return View();
+        }
+    }
+}
